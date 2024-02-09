f9187ece61c6af0f3c5b9e83ccba5a1a68bf5e9a
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-02-09 19:15:23 +0400

    Use react-query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,96 @@
+'use client';
+
 import Navbar from '@/components/Navbar';
+import axios from 'axios';
 import Image from 'next/image';
+import { useQuery } from 'react-query';
+
+interface WeatherInfo {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface MainInfo {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  sea_level: number;
+  grnd_level: number;
+  humidity: number;
+  temp_kf: number;
+}
+
+interface CloudInfo {
+  all: number;
+}
+
+interface WindInfo {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface SystemInfo {
+  pod: string;
+}
+
+interface ForecastItem {
+  dt: number;
+  main: MainInfo;
+  weather: WeatherInfo[];
+  clouds: CloudInfo;
+  wind: WindInfo;
+  visibility: number;
+  pop: number;
+  sys: SystemInfo;
+  dt_txt: string;
+}
+
+interface CityInfo {
+  id: number;
+  name: string;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: ForecastItem[];
+  city: CityInfo;
+}
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    'repoData',
+    async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      );
+
+      return data;
+    }
+  );
+
+  if (isLoading)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
