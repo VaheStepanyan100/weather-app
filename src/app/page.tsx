87022e7625b95df5daa6eb665c88b39c80c60fcb
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-02-18 13:06:48 +0400

    Add weather details to the main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,8 +103,8 @@ export default function Home() {
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
       <main className="px-3 max-w-7xl max-auto flex flex-col gap-9 w-full pb-10 pt-4">
-        <section>
-          <div>
+        <section className="space-y-4">
+          <div className="space-y-2">
             <h2 className="flex gap-1 text-2xl items-end">
               <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
               <p className="text-lg">
@@ -124,14 +124,27 @@ export default function Home() {
                 </p>
                 <p className="text-xs space-x-2">
                   <span>
-                  {convertCelvinToCelsius(firstData?.main.temp_min ?? 0)}
-                  ˚↓{' '}
+                    {convertCelvinToCelsius(firstData?.main.temp_min ?? 0)}
+                    ˚↓{' '}
                   </span>
                   <span>
-                  {convertCelvinToCelsius(firstData?.main.temp_max ?? 0)}
-                  ˚↑{' '}
+                    {convertCelvinToCelsius(firstData?.main.temp_max ?? 0)}
+                    ˚↑{' '}
                   </span>
                 </p>
+              </div>
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">
+                      {format(parseISO(d.dt_txt), 'h:mm a')}
+                    </p>
+                    <p>{convertCelvinToCelsius(d?.main.temp ?? 0)}˚</p>
+                  </div>
+                ))}
               </div>
             </Container>
           </div>
