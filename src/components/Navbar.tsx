866120a2ddbdecf6e7bfb6f9f7e18c9865b7600a
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-03-03 00:00:01 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,44 @@
-import { MdWbSunny } from "react-icons/md";
-import { MdOutlineMyLocation } from "react-icons/md";
-import { MdOutlineLocationOn } from "react-icons/md";
-import SearchBox from "./SearchBox";
+'use client';
+
+import { MdWbSunny } from 'react-icons/md';
+import { MdOutlineMyLocation } from 'react-icons/md';
+import { MdOutlineLocationOn } from 'react-icons/md';
+import SearchBox from './SearchBox';
+import { useState } from 'react';
+import axios from 'axios';
 
 type Props = {};
 
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
+
 export default function Navbar({}: Props) {
+  const [city, setCity] = useState('');
+  const [error, setError] = useState('');
+
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  async function hangleInputChange(value: string) {
+    setCity(value);
+    if (value.length >= 3) {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/find?q=${value}&appid=${API_KEY}`
+        );
+        const suggestions = response.data.list.map((item: any) => item.name);
+        setSuggestions(suggestions);
+        setError('');
+        setShowSuggestions(true);
+      } catch {
+        setSuggestions([]);
+        setShowSuggestions(false);
+      }
+    } else {
+      setSuggestions([]);
+      setShowSuggestions(false);
+    }
+  }
+
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
@@ -14,15 +47,41 @@ export default function Navbar({}: Props) {
           <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
         </p>
         <section className="flex gap-2 items-center">
-        <MdOutlineMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
-        <MdOutlineLocationOn className="text-3xl" />
-        <p className="text-state-900/80 text-sm">India</p>
-        <div>
-          <SearchBox />
-        </div>
+          <MdOutlineMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdOutlineLocationOn className="text-3xl" />
+          <p className="text-state-900/80 text-sm">India</p>
+          <div className="relative">
+            <SearchBox
+              value={city}
+              // onSubmit={}
+              onChange={(e) => hangleInputChange(e.target.value)}
+            />
+            <SuggestionBox />
+          </div>
         </section>
       </div>
     </nav>
+  );
+}
+
+function SuggestionBox({
+  showSuggestions,
+  suggestions,
+  handleSuggestionClick,
+  error,
+}: {
+  showSuggestions: boolean;
+  suggestions: string[];
+  handleSuggestionClick: (item: string) => void;
+  error: string;
+}) {
+  return (
+    <>
+      {' '}
+      <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
+        <li className="cursor-pointer p-1 rounded hover:bg-gray-200"></li>
+      </ul>
+    </>
   );
 }
 
