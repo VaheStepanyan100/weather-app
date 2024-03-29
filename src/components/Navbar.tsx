1f9528bda75487b5da1cc8516ccf8c2dba6408c8
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-03-29 11:22:12 +0400

    Add mobile responsiveness and implement current location functionality for Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MdWbSunny } from 'react-icons/md';
+import { MdMyLocation, MdWbSunny } from 'react-icons/md';
 import { MdOutlineMyLocation } from 'react-icons/md';
 import { MdOutlineLocationOn } from 'react-icons/md';
 import SearchBox from './SearchBox';
@@ -65,7 +65,28 @@ export default function Navbar({ location }: Props) {
     }
   }
 
+  function handleCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        try {
+          setLoadingCity(true);
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+          );
+          setTimeout(() => {
+            setLoadingCity(false);
+            setPlace(response.data.name);
+          }, 500);
+        } catch (error) {
+          setLoadingCity(false);
+        }
+      });
+    }
+  }
+
   return (
+    <>
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
         <p className="flex items-center justify-center gap-2">
@@ -73,10 +94,14 @@ export default function Navbar({ location }: Props) {
           <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
         </p>
         <section className="flex gap-2 items-center">
-          <MdOutlineMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdMyLocation
+            title="Your Current Location"
+            onClick={handleCurrentLocation}
+            className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer"
+          />
           <MdOutlineLocationOn className="text-3xl" />
           <p className="text-state-900/80 text-sm">{location}</p>
-          <div className="relative">
+          <div className="relative hidden md:flex">
             <SearchBox
               value={city}
               onSubmit={handleSubmitSearch}
@@ -94,6 +119,24 @@ export default function Navbar({ location }: Props) {
         </section>
       </div>
     </nav>
+    <section className='flex max-w-7xl px-3 md:hidden'>
+    <div className="relative">
+            <SearchBox
+              value={city}
+              onSubmit={handleSubmitSearch}
+              onChange={(e) => hangleInputChange(e.target.value)}
+            />
+            <SuggestionBox
+              {...{
+                showSuggestions,
+                suggestions,
+                handleSuggestionClick,
+                error,
+              }}
+            />
+          </div>
+          </section>
+    </>
   );
 }
 
